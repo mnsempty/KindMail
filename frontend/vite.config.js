7f8e09f177e8eb3ca3c93e7e295e396e65fc3c10
M-nogales <manuelnogalesserrano5221@gmail.com>
2024-03-16 00:25:18 +0100

    type added in package.json + server.js with redis, socket.io(no conex in this proyect),cors (for socket.io with react)+     synchronized backend and frontend (/backend)+ frontend now in port 3000

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, //? note cambiado a 3000, 5173? are u ok vite??
+    //todo with.env
+    proxy: {
+      //* note Redirige todas las solicitudes que comienzan con /backend a localhost:5000
+      '/backend': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/backend/, '')
+      }
+    }
+ }
 })
