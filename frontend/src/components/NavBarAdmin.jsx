418b5aed2acd6df8c2da78ca5f2fb3a6b6a1b13b
martaboso98 <frboruz@alumnos.ilernasevilla.com>
2024-03-19 11:45:45 +0100

    arreglos navbar

--- frontend/src/components/NavBarAdmin.jsx
+++ frontend/src/components/NavBarAdmin.jsx
@@ -74,10 +74,10 @@ export default function NavBarAdmin() {
                                         leaveTo="transform opacity-0 scale-95">
                                         <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-blanco py-1 shadow-lg ring-1 ring-negro ring-opacity-5 focus:outline-none">
                                             <Menu.Item>
-                                                {({ active }) => (
+                                                {/* {({ active }) => (
                                                     <a href="#" className={(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-negro')}>
                                                         Perfil</a>
-                                                )}
+                                                )} */}
                                             </Menu.Item>
                                             <Menu.Item>
                                                 {({ active }) => (
