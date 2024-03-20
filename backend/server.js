89aa98b5885d0382cb75183fb6dd7419d2945986
M-nogales <manuelnogalesserrano5221@gmail.com>
2024-03-20 21:09:03 +0100

    lil update

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const cors = require("cors");
 
 const { Server } = require("socket.io");
