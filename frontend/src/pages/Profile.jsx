0a7fa35c4e1c39a073fa371b84b44741ed6ca0a3
Álvaro <alvaro.martinez.lineros@gmail.com>
2024-03-21 13:03:42 +0100

    Inicio pagina de perfil (vista)

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,35 @@
+import { Avatar, AvatarGroup, AvatarIcon } from "@nextui-org/react";
+
 const Profile = () => {
     return (
-        <>Profile Page</>
+        <>
+            <div className="bg-azul grid grid-rows-2 md:grid-rows-2 gap-20 rounded-lg p-5">
+                <div className="grid grid-cols-1 md:grid-cols-2">
+                    <div className="flex justify-center md:justify-start">
+                        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026708c" className="w-50 h-50 text-large md:row-span-2 md:col-span-1" />
+                    </div>
+                    <div className="grid-rows-3">
+                        <p className="font-sans text-gray-50 text-center mt-2">Nombre User</p>
+                        <p className="font-sans text-gray-50 text-center mt-2">Email User</p>
+                    </div>
+                </div>
+
+
+                <div className="container flex justify-center md:justify-end">
+                    <button
+                        className="w-50 h-20 border-2 border-blanco text-blanco rounded-full px-12 py-2 inline-block font-semibold hover:bg-azulclaro hover:text-azul mb-10"
+                    >
+                        Cambiar datos
+                    </button>
+                </div>
+
+
+
+
+            </div>
+
+
+        </>
     )
 }
 
