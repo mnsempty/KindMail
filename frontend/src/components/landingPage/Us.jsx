c15f32b1d99b312d4d962b36d1aa6be4c753e3aa
martaboso98 <frboruz@alumnos.ilernasevilla.com>
2024-04-30 19:12:13 +0200

    cambio imagenes landing

--- frontend/src/components/landingPage/Us.jsx
+++ frontend/src/components/landingPage/Us.jsx
@@ -1,98 +1,50 @@
 import React from "react";
 import { Image } from "@nextui-org/react";
-import image1 from "../../assets/6326055.png";
-// import image2 from "";
-// import image3 from "";
-// import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
-// import {
-//     Card,
-//     CardHeader,
-//     CardBody,
-//     CardFooter,
-//     Typography,
-//     Tooltip,
-// } from "@material-tailwind/react";
+import image1 from "../../assets/manu.png";
+import image2 from "../../assets/marta.png";
+import image3 from "../../assets/manu.png";
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    Typography,
+} from "@material-tailwind/react";
 
 const Us = () => {
     return (
-        <div className="flex justify-evenly">
-            <Image isZoomed src={image1} alt="Álvaro" className="h-96" />
-            <Image isZoomed src={image1} alt="Marta" className="h-96" />
-            <Image isZoomed src={image1} alt="Manuel" className="h-96" />
-            {/* <Card className="w-96">
-                <CardHeader floated={false} className="h-96">
-                    <Image isZoomed src={image1} alt="Álvaro" />
+        <div className="flex flex-wrap justify-evenly">
+            <Card className="w-full md:w-96 m-2">
+                <CardHeader className="h-72 md:h-96">
+                    <img isZoomed src={image1} alt="Álvaro" />
                 </CardHeader>
                 <CardBody className="text-center py-5">
                     <Typography variant="h4" color="blue-gray" className="mb-2">
                         Álvaro Martínez Lineros
                     </Typography>
-                    <Typography color="blue-gray" className="font-medium" textGradient>
-                        SAP (Deloitte)
-                    </Typography>
                 </CardBody>
-                <CardFooter className="flex justify-center gap-7 py-5">
-                    <Tooltip content="GitHub">
-                        <FaGithub className="text-azul" href="#" size={24} />
-                    </Tooltip>
-                    <Tooltip content="Linkedin">
-                        <FaLinkedin className="text-azul" href="#" size={24} />
-                    </Tooltip>
-                    <Tooltip content="Instagram">
-                        <FaInstagram className="text-azul" href="#" size={24} />
-                    </Tooltip>
-                </CardFooter>
             </Card>
 
-            <Card className="w-96">
-                <CardHeader floated={false} className="h-96">
-                    <Image isZoomed src={image2} alt="Álvaro" />
+            <Card className="w-full md:w-96 m-2">
+                <CardHeader className="h-72 md:h-96">
+                    <img isZoomed src={image2} alt="Álvaro" />
                 </CardHeader>
                 <CardBody className="text-center py-5">
                     <Typography variant="h4" color="blue-gray" className="mb-2">
                         Marta Borreguero Soria
                     </Typography>
-                    <Typography color="blue-gray" className="font-medium" textGradient>
-                        Front (Deloitte)
-                    </Typography>
                 </CardBody>
-                <CardFooter className="flex justify-center gap-7 py-5">
-                    <Tooltip content="GitHub">
-                        <FaGithub className="text-azul" href="#" size={24} />
-                    </Tooltip>
-                    <Tooltip content="Linkedin">
-                        <FaLinkedin className="text-azul" href="#" size={24} />
-                    </Tooltip>
-                    <Tooltip content="Instagram">
-                        <FaInstagram className="text-azul" href="#" size={24} />
-                    </Tooltip>
-                </CardFooter>
             </Card>
 
-            <Card className="w-96">
-                <CardHeader floated={false} className="h-96">
-                    <Image isZoomed src={image3} alt="Álvaro" />
+            <Card className="w-full md:w-96 m-2">
+                <CardHeader className="h-72 md:h-96">
+                    <img isZoomed src={image3} alt="Álvaro" />
                 </CardHeader>
                 <CardBody className="text-center py-5">
                     <Typography variant="h4" color="blue-gray" className="mb-2">
                         Manuel Nogales Serrano
                     </Typography>
-                    <Typography color="blue-gray" className="font-medium" textGradient>
-                        Oracle (Ayesa)
-                    </Typography>
                 </CardBody>
-                <CardFooter className="flex justify-center gap-7 py-5">
-                    <Tooltip content="GitHub">
-                        <FaGithub className="text-azul" href="#" size={24} />
-                    </Tooltip>
-                    <Tooltip content="Linkedin">
-                        <FaLinkedin className="text-azul" href="#" size={24} />
-                    </Tooltip>
-                    <Tooltip content="Instagram">
-                        <FaInstagram className="text-azul" href="#" size={24} />
-                    </Tooltip>
-                </CardFooter>
-            </Card> */}
+            </Card>
         </div >
     );
 };
